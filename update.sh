c29e1f555b4057ba95b40e06748e1204d85af51e
Dylan Bienenstock <dylanbienenstock@gmail.com>
2019-02-04 21:20:23 -0800

    Add update script

---
+++ update.sh
@@ -0,0 +1,34 @@
+echo ""
+echo "####################################################"
+echo "### Pulling most recent version from remote repo ###"
+echo "####################################################"
+
+git reset --hard
+git pull
+
+echo ""
+echo "#########################"
+echo "### Rebuilding client ###"
+echo "#########################"
+
+cd client
+npm install
+npm build
+
+echo ""
+echo "#########################"
+echo "### Rebuilding server ###"
+echo "#########################"
+
+cd ../server
+npm install
+tsc
+
+echo ""
+echo "###############################"
+echo "### Restarting node service ###"
+echo "###############################"
+
+service node restart
+
+echo "Done!"
